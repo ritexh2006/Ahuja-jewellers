cdd7e86cac8b9e2b563d2f3a9b03b1d0deca01f6
ritexh2006 <ritexh2006@gmail.com>
2024-07-04 12:47:51 +0530

    New Changes

--- script.js
+++ script.js
@@ -36,63 +36,63 @@ document.addEventListener("mousemove",function(dets){
 })
 
 
-//section-3
+// //section-3
 
-gsap.from(".section-3 h1,.section-3 h2", {
-    y: 10,
-    rotate: 10,
-    opacity: 0,
-    delay: 0.3,
-    duration: 0.7
-})
-var tl = gsap.timeline({
-    scrollTrigger: {
-        trigger: ".section-3 h1",
-        scroller: ".main",
-        // markers:true,
-        start: "top 27%",
-        end: "top 0",
-        scrub: 3
-    }
-})
-tl.to(".section-3 h1", {
-    x: -100,
-}, "anim")
-tl.to(".section-3 h2", {
-    x: 100
-}, "anim")
-tl.to(".section-3 video", {
-    width: "90%"
-}, "anim")
+// gsap.from(".section-3 h1,.section-3 h2", {
+//     y: 10,
+//     rotate: 10,
+//     opacity: 0,
+//     delay: 0.3,
+//     duration: 0.7
+// })
+// var tl = gsap.timeline({
+//     scrollTrigger: {
+//         trigger: ".section-3 h1",
+//         scroller: ".main",
+//         // markers:true,
+//         start: "top 27%",
+//         end: "top 0",
+//         scrub: 3
+//     }
+// })
+// tl.to(".section-3 h1", {
+//     x: -100,
+// }, "anim")
+// tl.to(".section-3 h2", {
+//     x: 100
+// }, "anim")
+// tl.to(".section-3 video", {
+//     width: "90%"
+// }, "anim")
 
-var tl2 = gsap.timeline({
-    scrollTrigger: {
-        trigger: ".section-3 h1",
-        scroller: ".main",
-        // markers:true,
-        start: "top -115%",
-        end: "top -120%",
-        scrub: 3
-    }
-})
-tl2.to(".main", {
-    backgroundColor: "#fff",
-})
+// var tl2 = gsap.timeline({
+//     scrollTrigger: {
+//         trigger: ".section-3 h1",
+//         scroller: ".main",
+//         // markers:true,
+//         start: "top -115%",
+//         end: "top -120%",
+//         scrub: 3
+//     }
+// })
+// tl2.to(".main", {
+//     backgroundColor: "#fff",
+// })
 
-var tl3 = gsap.timeline({
-    scrollTrigger: {
-        trigger: ".section-3 h1",
-        scroller: ".main",
-        // markers:true,
-        start: "top -280%",
-        end: "top -300%",
-        scrub: 3
-    }
-})
+// var tl3 = gsap.timeline({
+//     scrollTrigger: {
+//         trigger: ".section-3 h1",
+//         scroller: ".main",
+//         // markers:true,
+//         start: "top -280%",
+//         end: "top -300%",
+//         scrub: 3
+//     }
+// })
 
-tl3.to(".main",{
-    backgroundColor:"#0F0D0D"
-})
+// tl3.to(".main",{
+//     backgroundColor:"#0F0D0D"
+// })
 
 
 
