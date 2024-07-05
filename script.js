3a68f9e903bfbebed3252e7bd171580bfaa4de90
ritexh2006 <ritexh2006@gmail.com>
2024-07-05 10:27:12 +0530

    Today Update

--- script.js
+++ script.js
@@ -1,34 +1,3 @@
-// function init() {
-//     gsap.registerPlugin(ScrollTrigger);
-
-//     const locoScroll = new LocomotiveScroll({
-//         el: document.querySelector(".main"),
-//         smooth: true
-//     });
-//     locoScroll.on("scroll", ScrollTrigger.update);
-
-//     ScrollTrigger.scrollerProxy(".main", {
-//         scrollTop(value) {
-//             return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
-//         }, // we don't have to define a scrollLeft because we're only scrolling vertically.
-//         getBoundingClientRect() {
-//             return { top: 0, left: 0, width: window.innerWidth, height: window.innerHeight };
-//         },
-//         pinType: document.querySelector(".main").style.transform ? "transform" : "fixed"
-//     });
-
-
-//     ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
-
-//     ScrollTrigger.refresh();
-
-// }
-
-// //cursor
-
-// init()
-
-
 const lenis = new Lenis()
 
 lenis.on('scroll', (e) => {
@@ -50,9 +19,7 @@ document.addEventListener("mousemove", function (dets) {
     crsr.style.top = dets.y + 20 + "px"
 })
 
-
 // //section-3
-
 gsap.from(".section-3 h1,.section-3 h2", {
     y: 10,
     rotate: 10,
@@ -65,8 +32,8 @@ var tl = gsap.timeline({
         trigger: ".section-3",
         // scroller: ".main",
         // markers: true,
-        start: "top 30%",
-        end: "100% 90%",
+        start: "top 40%",
+        end: "100% 40%",
         scrub: 3
     }
 })
@@ -79,38 +46,6 @@ tl.to(".section-3 h2", {
 tl.to(".section-3 video", {
     width: "90%"
 }, "anim")
-
-// var tl2 = gsap.timeline({
-//     scrollTrigger: {
-//         trigger: ".section-3 h1",
-//         // scroller: ".main",
-//         markers: true,
-//         start: "10% 50%",
-//         end: "70% 50%",
-//         scrub: true,
-//     }
-// })
-// tl2.to(".main", {
-//     backgroundColor: "#fff",
-// })
-
-// var tl3 = gsap.timeline({
-//     scrollTrigger: {
-//         trigger: ".section-3 h1",
-//         // scroller: ".main",
-//         markers: true,
-//         start: "10% 50%",
-//         end: "70% 50%",
-//         scrub: 3
-//     }
-// })
-
-// tl3.to(".main", {
-//     backgroundColor: "#0F0D0D"
-// })
-
-
-
 
 //navbar
 var slider = document.querySelector(".slider")
